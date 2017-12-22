57c49cfdfe319081ca4c75d67c3abfbc9d99a651
Zoltan Gyebrovszki <gyeby5@gmail.com>
2017-12-22 03:17:38 +0100

    Transformer project.

--- src/Transformer/Extensions.cs
+++ src/Transformer/Extensions.cs
@@ -9,18 +9,12 @@ namespace Transformer
 {
     static class Extensions
     {
-        public static EntryEnumerable<Q> ParseSequence<T, Q>(this EntryEnumerable<T> input,
-            Func<T, Q> startEntrySelector,
-            Func<T, string> scanner,
-            Func<T, Q, SequenceState> recorder) where T : Entry where Q : Entry
+        public static IEnumerable<TResult> Collect<TSource, TResult>(this IEnumerable<TSource> input, Func<TSource, Tuple<string, string>> keySelector, Func<TResult, TResult> finalizer = null) where TSource : Entry where TResult : EntryCollection, new()
         {
-            return new GenericSequenceParser<T, Q>(input, startEntrySelector, scanner, recorder);
+            var collector = new GenericCollector<TSource, TResult>(keySelector, finalizer);
+            collector.Initialize(input);
+            return collector;
         }
-        public static EntryEnumerable<Q> SequenceParser<T, Q>(this EntryEnumerable<T> input,
-            SequenceParser<T, Q> instance) where T : Entry where Q : Entry
-        {
-            instance.Initialize(input);
-            return instance;
-        }
+
     }
 }
--- src/Transformer/Program.cs
+++ src/Transformer/Program.cs
@@ -8,12 +8,61 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
-//UNDONE: XMLDOC 2
-
 namespace Transformer
 {
     class Program
     {
+        private class AppDomainSimplifier
+        {
+            private readonly string _format;
+            private List<string> _keys = new List<string>();
+
+            public AppDomainSimplifier(string format = null)
+            {
+                _format = format ?? "App-{0}";
+            }
+
+            public string Simplify(string key)
+            {
+                var i = _keys.IndexOf(key);
+                if (i < 0)
+                {
+                    i = _keys.Count;
+                    _keys.Add(key);
+                }
+                return string.Format(_format, (i + 1));
+            }
+        }
+        private class WebRequestEntryCollection : EntryCollection
+        {
+            public static class Q
+            {
+                public const string Start = "start";
+                public const string End = "end";
+            }
+
+            public Entry StartEntry;
+            public Entry EndEntry;
+
+            public override void Add(Entry e, string qualification)
+            {
+                switch (qualification)
+                {
+                    case Q.Start:
+                        StartEntry = e;
+                        break;
+                    case Q.End:
+                        EndEntry = e;
+                        break;
+                }
+            }
+
+            public override bool Finished()
+            {
+                return StartEntry != null && EndEntry != null;
+            }
+        }
+
         static void Main(string[] args)
         {
             var logDirectories = new[] {
@@ -22,13 +71,10 @@ namespace Transformer
             };
             var outputDir = @"C:\Users\Gyebi\Desktop\benchmark\02\";
 
-            //Test1(logDirectories, outputDir);
-
-            //Test2(logDirectories, outputDir);
-
-            Test3(logDirectories, outputDir);
-
-            //Test4(logDirectories, outputDir);
+            Test1_SimpleRead(logDirectories, outputDir);
+            Test2_Filtering(logDirectories, outputDir);
+            Test3_Modifying(logDirectories, outputDir);
+            Test4_Collecting(logDirectories, outputDir);
 
             if (Debugger.IsAttached)
             {
@@ -37,170 +83,74 @@ namespace Transformer
             }
         }
 
-        private static void Test1(string[] logDirectories, string outputDir)
+        private static void Test1_SimpleRead(string[] logDirectories, string outputDir)
         {
-            using (var writer = new StreamWriter(outputDir + "events.txt"))
-            using (var analyzator = Reader.Create(logDirectories, "detailedlog_*.log").Filter<Entry>(e => e.Category == "Event"))
+            using (var writer = new StreamWriter(Path.Combine(outputDir, "webrequests1.log")))
+            using (var logFlow = Reader.Create(logDirectories, "detailedlog_*.log"))
             {
-                foreach (var line in analyzator)
-                    writer.WriteLine(line);
+                var transformedLogFlow = logFlow
+                    .Take(10);
+                foreach (var item in transformedLogFlow)
+                    writer.WriteLine(item);
             }
         }
-
-        private static void Test2(string[] logDirectories, string outputDir)
+        private static void Test2_Filtering(string[] logDirectories, string outputDir)
         {
-            using (var writer = new StreamWriter(outputDir + "indexing.txt"))
-            using (var analyzator = Reader.Create(logDirectories, "detailedlog_*.log")
-                .Filter<Entry>(e => e.Category == "Index" && e.Message.EndsWith("Indexing node") && e.Status == "End")
-                .Transform<Entry>(e => e.Duration.ToString().Replace("00:00:", "").Replace(".", ",")))
+            using (var writer = new StreamWriter(Path.Combine(outputDir, "webrequests2.log")))
+            using (var logFlow = Reader.Create(logDirectories, "detailedlog_*.log")            )
             {
-                foreach (var line in analyzator)
-                    writer.WriteLine(line);
+                var transformedLogFlow = logFlow
+                    .Where(e => e.Category == "Web")
+                    .Take(50);
+                foreach (var item in transformedLogFlow)
+                    writer.WriteLine(item);
             }
         }
-
-        private static void Test3(string[] logDirectories, string outputDir)
+        private static void Test3_Modifying(string[] logDirectories, string outputDir)
         {
-            using (var writer = new StreamWriter(outputDir + "indexing_debug.txt"))
-            using (var analyzator = Reader.Create(logDirectories, "detailedlog_*.log")
-                .Filter<Entry>(e => e.Category == "Index" || e.Category == "IndexQueue")
-                .Filter<Entry>(
-                    e => e.Message.StartsWith("ExecuteDistributedActivity:") ||
-                    e.Message.Contains("arrived from another computer.")))
+            using (var writer = new StreamWriter(Path.Combine(outputDir, "webrequests3.log")))
+            using (var logFlow = Reader.Create(logDirectories, "detailedlog_*.log"))
             {
-                writer.WriteLine($"MIN\tSEC\tAPP\tACTIVITY\tMESSAGE");
-                var activityIdRegex = new Regex(" [A#][0-9]+");
-                foreach (var item in analyzator)
+                var aps = new AppDomainSimplifier("AppDomain-{0}");
+
+                var transformedLogFlow = logFlow
+                    .Where(e => e.Category == "Web")
+                    .Select(e => { e.AppDomain = aps.Simplify(e.AppDomain); return e; })
+                    .Take(50);
+                foreach (var item in transformedLogFlow)
                 {
-                    // IAQ: A5142 arrived from another computer....
-                    // ExecuteDistributedActivity: #5143
-                    var regexMatch = activityIdRegex.Match(item.Message);
-                    var activityId = regexMatch.Success ? regexMatch.Value.Trim().Substring(1) : "?";
-                    writer.WriteLine($"{item.Time.ToString("mm\tss.fffff").Replace(".", ",")}\t{item.AppDomain}\tA{activityId}\t{item.Message}\t");
-                }
-            }
-        }
-
-        private static void Test4(string[] logDirectories, string outputDir)
-        {
-            string key;
-            var regex = new Regex("IAQ: A[0-9]+");
-            Match regexMatch;
-            using (var writer = new StreamWriter(outputDir + "indexing.txt"))
-            using (var analyzator = Reader.Create(logDirectories, "detailedlog_*.log")
-                .Filter<Entry>(e => e.Category == "Index" || e.Category == "IndexQueue")
-                .ParseSequence<Entry, DistributedIndexingActivityEntry>(
-                    startEntrySelector: (e) => {
-                        if (e.Message.StartsWith("ExecuteDistributedActivity: #"))
-                            return new DistributedIndexingActivityEntry(e);
-                        return null;
-                    },
-                    scanner: (e) => {
-                        if (e.Message.StartsWith("ExecuteDistributedActivity: #"))
-                        {
-                            key = e.Message.Replace("ExecuteDistributedActivity: #", "A");
-                            return key;
-                        }
-
-                        regexMatch = regex.Match(e.Message);
-                        if (regexMatch.Success)
-                        {
-                            key = (regexMatch.Value.Substring("IAQ: ".Length));
-                            return key;
-                        }
-
-                        return null;
-                    },
-                    recorder: (entry, record) => {
-                        if (entry.Message.Contains("arrived from another computer."))
-                        {
-                            record.ArrivedOnOther = entry;
-                        }
-                        else if (entry.Message.EndsWith("dequeued."))
-                        {
-                            if (entry.AppDomain == record.AppDomain)
-                                record.DequeuedOnSame = entry;
-                            else
-                                record.DequeuedOnOther = entry;
-                        }
-                        else if (entry.Message.EndsWith("EXECUTION."))
-                        {
-                            if (entry.AppDomain == record.AppDomain)
-                            {
-                                if (entry.Status == "Start")
-                                    record.StartOnSame = entry;
-                                else
-                                    record.EndOnSame = entry;
-                            }
-                            else
-                            {
-                                if (entry.Status == "Start")
-                                    record.StartOnOther = entry;
-                                else
-                                    record.EndOnOther = entry;
-                            }
-                        }
-
-                        return (record.EndOnSame != null && record.EndOnOther != null)
-                            ? SequenceState.Finished
-                            : SequenceState.Recording;
-                    }
-                )
-                .Transform<Entry>((r) => {
-                    if (r.ArrivedOnOther == null)
-                    {
-                        int q = 1;
-                    }
-                    var t0 = (r.DequeuedOnSame.Time - r.Time).Milliseconds;
-                    var t1 = (r.StartOnSame.Time - r.DequeuedOnSame.Time).Milliseconds;
-                    var t2 = (r.EndOnSame.Time - r.StartOnSame.Time).Milliseconds;
-                    var t3 = (r.ArrivedOnOther.Time - r.Time).Milliseconds;
-                    var t4 = (r.DequeuedOnOther.Time - r.ArrivedOnOther.Time).Milliseconds;
-                    var t5 = (r.StartOnOther.Time - r.DequeuedOnOther.Time).Milliseconds;
-                    var t6 = (r.EndOnOther.Time - r.StartOnOther.Time).Milliseconds;
-                    return $"{r.AppDomain}\t{r.ActivityId}\t{t0}\t{t1}\t{t2}\t|\t{t3}\t{t4}\t{t5}\t{t6}";
-                })
-            )
-            {
-                var lastDisplay = DateTime.Now;
-                var waitTime = TimeSpan.FromSeconds(1);
-                foreach (var item in analyzator)
-                {
-                    if (DateTime.Now - lastDisplay > waitTime)
-                    {
-                        Console.Write(".");
-                        lastDisplay = DateTime.Now;
-                    }
                     writer.WriteLine(item);
                 }
-                Console.WriteLine();
             }
         }
-
-        //       ExecuteDistributedActivity: #6607
-        //       IAQ: A6607 dequeued.
-        // Start IAQ: A6607 EXECUTION.
-        // End   IAQ: A6607 EXECUTION.
-        // -----------------------------
-        //       IAQ: A6607 arrived from another computer.
-        //       IAQ: A6607 dequeued.
-        // Start IAQ: A6607 EXECUTION.
-        // End   IAQ: A6607 EXECUTION.
-
-        private class DistributedIndexingActivityEntry : Entry
+        private static void Test4_Collecting(string[] logDirectories, string outputDir)
         {
-            public string ActivityId { get; }
-            public Entry DequeuedOnSame { get; internal set; }
-            public Entry StartOnSame { get; internal set; }
-            public Entry EndOnSame { get; internal set; }
-            public Entry ArrivedOnOther { get; internal set; }
-            public Entry DequeuedOnOther { get; internal set; }
-            public Entry StartOnOther { get; internal set; }
-            public Entry EndOnOther { get; internal set; }
-
-            public DistributedIndexingActivityEntry(Entry sourceEntry) : base(sourceEntry)
+            using (var writer = new StreamWriter(Path.Combine(outputDir, "webrequests4.log")))
+            using (var logFlow = Reader.Create(logDirectories, "detailedlog_*.log"))
             {
-                ActivityId = sourceEntry.Message.Replace("ExecuteDistributedActivity: #", "A");
+                var aps = new AppDomainSimplifier("AppDomain-{0}");
+
+                var transformedLogFlow = logFlow
+                    .Where(e => e.Category == "Web")
+                    .Select(e => { e.AppDomain = aps.Simplify(e.AppDomain); return e; })
+                    .Collect<Entry, WebRequestEntryCollection>((e) =>
+                    {
+                        if (e.Message.StartsWith("PCM.OnEnter "))
+                            return new Tuple<string, string>($"{e.AppDomain}|{e.ThreadId}|{e.Message.Substring("PCM.OnEnter ".Length)}", WebRequestEntryCollection.Q.Start);
+                        else if (e.Message.StartsWith("PCM.OnEndRequest "))
+                            return new Tuple<string, string>($"{e.AppDomain}|{e.ThreadId}|{e.Message.Substring("PCM.OnEndRequest ".Length)}", WebRequestEntryCollection.Q.End);
+                        return null;
+                    })
+                    .Take(50);
+                foreach (var item in transformedLogFlow)
+                {
+                    var app = item.StartEntry.AppDomain;
+                    var time = item.StartEntry.Time.ToString("HH:mm:ss.fffff");
+                    var req = item.StartEntry.Message.Substring("PCM.OnEnter ".Length);
+                    var dt = item.EndEntry.Time - item.StartEntry.Time;
+                    writer.WriteLine($"{app}\t{time}\t{dt}\t{req}");
+                }
+                //var collections = analyzator.Context.Collections;
             }
         }
     }
